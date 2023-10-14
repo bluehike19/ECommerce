830ea74c43463b7a2795547d82a8018ac5e519da
simo <wambuisimon712@gmail.com>
2023-10-14 12:32:01 +0300

    new

--- frontend/src/components/leftbar/LeftBar.jsx
+++ frontend/src/components/leftbar/LeftBar.jsx
@@ -1,150 +1,90 @@
 import "./leftBar.scss";
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Gallery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Tutorials from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import Fund from "../../assets/13.png";
+import { AuthContext } from "../../context/authContext";
+import { useContext } from "react";
 
 const LeftBar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="leftBar">
       <div className="container">
         <div className="menu">
           <div className="user">
-            <img
-              src="23528.jpg"
-              style={{
-                width: "30px",
-                height: "30px",
-                borderRadius: "50%",
-                objectFit: "cover",
-              }}
-              alt="simoblues"
-            />
-            <span>simo blues</span>
+            <img src={currentUser.profilePic} alt="" />
+            <span>{currentUser.name}</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
+            <img src={Friends} alt="" />
             <span>Friends</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
+            <img src={Groups} alt="" />
             <span>Groups</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Market Place</span>
+            <img src={Market} alt="" />
+            <span>Marketplace</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
+            <img src={Watch} alt="" />
             <span>Watch</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Memorries</span>
+            <img src={Memories} alt="" />
+            <span>Memories</span>
           </div>
         </div>
         <hr />
         <div className="menu">
-          <span>Your Shortcut</span>
-
+          <span>Your shortcuts</span>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Friends</span>
+            <img src={Events} alt="" />
+            <span>Events</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Groups</span>
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Market Place</span>
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Watch</span>
+            <img src={Videos} alt="" />
+            <span>Videos</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Memorries</span>
+            <img src={Messages} alt="" />
+            <span>Messages</span>
           </div>
         </div>
         <hr />
         <div className="menu">
+          <span>Others</span>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Friends</span>
+            <img src={Fund} alt="" />
+            <span>Fundraiser</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Groups</span>
+            <img src={Tutorials} alt="" />
+            <span>Tutorials</span>
           </div>
           <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Market Place</span>
-          </div>
-          <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Watch</span>
-          </div>
-          <div className="item">
-            <img
-              src="car.jpg"
-              style={{ width: "30px", height: "30px", borderRadius: "50%" }}
-              alt=""
-            />
-            <span>Memorries</span>
+            <img src={Courses} alt="" />
+            <span>Courses</span>
           </div>
         </div>
       </div>
--- frontend/src/components/rightbar/RightBar.jsx
+++ frontend/src/components/rightbar/RightBar.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react";
 import "./rightBar.scss";
-import { AuthContext } from "../../context/authContext";
 
 const RightBar = () => {
-  const { currentUser } = useContext(AuthContext);
-
   return (
     <div className="rightBar">
       <div className="container">
@@ -12,8 +8,11 @@ const RightBar = () => {
           <span>Suggestions For You</span>
           <div className="user">
             <div className="userInfo">
-              <img src={currentUser.profilePic} alt="" />
-              <span>{currentUser.name}</span>
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Jane Doe</span>
             </div>
             <div className="buttons">
               <button>follow</button>
@@ -22,8 +21,11 @@ const RightBar = () => {
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
-              <span>simo blues</span>
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Jane Doe</span>
             </div>
             <div className="buttons">
               <button>follow</button>
@@ -35,42 +37,48 @@ const RightBar = () => {
           <span>Latest Activities</span>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <p>
-                <span>simoblues </span>
-                <span> changed their cover picture</span>
-              </p>
-            </div>
-            <div className="buttons">
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img src="simo.jpg" alt="" />
-              <p>
-                <span>simo blues</span>
-                changed their cover picture
+                <span>Jane Doe</span> changed their cover picture
               </p>
             </div>
             <span>1 min ago</span>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <p>
-                <span>simo blues</span>
-                changed their cover picture
+                <span>Jane Doe</span> changed their cover picture
               </p>
             </div>
             <span>1 min ago</span>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <p>
-                <span>simo blues</span>
-                changed their cover picture
+                <span>Jane Doe</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <p>
+                <span>Jane Doe</span> changed their cover picture
               </p>
             </div>
             <span>1 min ago</span>
@@ -80,32 +88,112 @@ const RightBar = () => {
           <span>Online Friends</span>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="online" />
+              <span>Jane Doe</span>
             </div>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="online" />
+              <span>Jane Doe</span>
             </div>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="online" />
+              <span>Jane Doe</span>
             </div>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="online" />
+              <span>Jane Doe</span>
             </div>
           </div>
           <div className="user">
             <div className="userInfo">
-              <img src="simo.jpg" alt="" />
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Jane Doe</span>
             </div>
           </div>
         </div>
@@ -115,3 +203,121 @@ const RightBar = () => {
 };
 
 export default RightBar;
+
+// import { useContext } from "react";
+// import "./rightBar.scss";
+// import { AuthContext } from "../../context/authContext";
+
+// const RightBar = () => {
+//   const { currentUser } = useContext(AuthContext);
+
+//   return (
+//     <div className="rightBar">
+//       <div className="container">
+//         <div className="item">
+//           <span>Suggestions For You</span>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src={currentUser.profilePic} alt="" />
+//               <span>{currentUser.name}</span>
+//             </div>
+//             <div className="buttons">
+//               <button>follow</button>
+//               <button>dismiss</button>
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <span>simo blues</span>
+//             </div>
+//             <div className="buttons">
+//               <button>follow</button>
+//               <button>dismiss</button>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="item">
+//           <span>Latest Activities</span>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <p>
+//                 <span>simoblues </span>
+//                 <span> changed their cover picture</span>
+//               </p>
+//             </div>
+//             <div className="buttons">
+//               <span>1 min ago</span>
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <p>
+//                 <span>simo blues</span>
+//                 changed their cover picture
+//               </p>
+//             </div>
+//             <span>1 min ago</span>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <p>
+//                 <span>simo blues</span>
+//                 changed their cover picture
+//               </p>
+//             </div>
+//             <span>1 min ago</span>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <p>
+//                 <span>simo blues</span>
+//                 changed their cover picture
+//               </p>
+//             </div>
+//             <span>1 min ago</span>
+//           </div>
+//         </div>
+//         <div className="item">
+//           <span>Online Friends</span>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <div className="online" />
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <div className="online" />
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <div className="online" />
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <div className="online" />
+//             </div>
+//           </div>
+//           <div className="user">
+//             <div className="userInfo">
+//               <img src="simo.jpg" alt="" />
+//               <div className="online" />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RightBar;
